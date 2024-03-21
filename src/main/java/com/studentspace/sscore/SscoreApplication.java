e8462ee3281c08dd81fecad1f0cf735a31715865
marcoslafoz <marcos.lafoz96@gmail.com>
2024-03-21 18:49:00 +0100

    First commit

---
+++ src/main/java/com/studentspace/sscore/SscoreApplication.java
@@ -0,0 +1,13 @@
+package com.studentspace.sscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentspace/sscore/SscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentspace.sscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
