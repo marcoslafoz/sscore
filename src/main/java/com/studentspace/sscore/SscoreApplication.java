6aba10d4ad2829ad2d1165fd7ed9fe72755d10a1
marcoslafoz <marcos.lafoz96@gmail.com>
2024-04-23 23:30:42 +0200

    .

--- src/main/java/com/studentspace/sscore/SscoreApplication.java
+++ src/main/java/com/studentspace/sscore/SscoreApplication.java
@@ -1,8 +1,6 @@
 package com.studentspace.sscore;
 
-import com.studentspace.sscore.model.User;
-import com.studentspace.sscore.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,9 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan
 public class SscoreApplication {
-
-	@Autowired
-	private UserService userService;
 
 	public static void main(String[] args) {
 
