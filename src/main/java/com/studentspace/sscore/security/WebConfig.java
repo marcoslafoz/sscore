7c8c3f5ec1937c5d7e50d8e55b1fc655dc11afb8
marcoslafoz <marcos.lafoz96@gmail.com>
2024-04-28 19:28:16 +0200

    WebConfig fix

--- src/main/java/com/studentspace/sscore/security/WebConfig.java
+++ src/main/java/com/studentspace/sscore/security/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // IP
+                .allowedOrigins("localhost*") // IP
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
